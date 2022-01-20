5eb0bbd42c76e90eab81aea2a6345742e8920d9c
Fortunate ogodu <ogodujoseph@gmail.com>
2022-01-20 12:13:26 +0100

    update

--- src/components/ContentBox.jsx
+++ src/components/ContentBox.jsx
@@ -17,12 +17,12 @@ const ContentBox = () => {
           <p className="">Joined 26 Jan 2011</p>
         </div>
       </div>
-      <p className="my-8">
+      <p className="my-10">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
         rerum dolore incidunt quis nobis impedit laudantium enim omnis velit
         hic!
       </p>
-      <div className="bg-body px-2 py-3 rounded-lg flex justify-between items-center text-center mb-4">
+      <div className="bg-body px-4 py-3 rounded-lg flex justify-between items-center text-center mb-8">
         <div className="">
           <h3>Repos</h3>
           <h2 className="text-white font-bold text-base">8</h2>
@@ -36,18 +36,18 @@ const ContentBox = () => {
           <h2 className="text-white font-bold text-base">200</h2>
         </div>
       </div>
-      <div className="space-y-3">
+      <div className="space-y-5">
         <div className="flex item-center">
           <MdLocationOn className="text-base mr-3" />
           <span>San Francisco</span>
         </div>
         <div className="flex item-center">
           <FiLink className="text-base mr-3" />
-          <span>https://github.blog</span>
+          <span className="text-blue-500">https://github.blog</span>
         </div>
         <div className="flex item-center">
           <FaTwitter className="text-base mr-3" />
-          <span>@iamFortunate</span>
+          <span className="text-blue-500">@iamfortunate</span>
         </div>
         <div className="flex item-center">
           <BsLinkedin className="text-base mr-3" />
